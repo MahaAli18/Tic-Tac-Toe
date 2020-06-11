11d73f88e7eff889e4b586eee4a3bfbca7a6609e
MahaAli18 <43891334+MahaAli18@users.noreply.github.com>
2020-06-11 14:12:10 +0500

    handle turn

--- main.py
+++ main.py
@@ -10,20 +10,27 @@ def display_board():
 
 
 def play_game():
-
     #Display the board
     display_board()
 
-    #handle turn
-    handle_turn()
+    while game_still_going:
+
+        handle_turn(current_player)
+
+        check_if_game_over()
+
+        flip_player()
+
+   
 
 
 def handle_turn():
     position = input("choose a position from 1-9: ") 
     position = int(position) - 1
 
-    board[position] = "x"
+    board[position] = "X"
     display_board()
 
 
+def check_if_game_over()
 play_game()    
