361e476b41f1ee99f2ed18fb77bb6c99cc4dab6b
MahaAli18 <43891334+MahaAli18@users.noreply.github.com>
2020-06-11 15:09:34 +0500

    check if the game is still going

--- main.py
+++ main.py
@@ -20,18 +20,28 @@ def display_board():
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
 
-
+#play game of tic tac toe
 def play_game():
     #Display the board
     display_board()
 
+    #while the game is still going
     while game_still_going:
-
+        
+        #handle turn to an arbitrary player
         handle_turn(current_player)
-
+        
+        #check if game is ended
         check_if_game_over()
-
+         
+        #Flip turns
         flip_player()
+
+    #The game has ended
+    if winner == "X" or winner == "O":
+       print(winner + " won.")
+    elif winner == None:
+       print("Tie.")      
 
    
 
